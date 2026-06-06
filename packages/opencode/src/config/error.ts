c1b63d197d380de50cba4de7ebcf20b73c59b502
chengda <chengda@joysfintech.com>
2026-06-06 16:36:43 +0800

    chore(config): add error module with re-exports from core

---
+++ packages/opencode/src/config/error.ts
@@ -0,0 +1,5 @@
+export * as ConfigError from "./error"
+
+import { InvalidError, JsonError, FrontmatterError, DirectoryTypoError } from "@opencode-ai/core/v1/config/error"
+
+export { InvalidError, JsonError, FrontmatterError, DirectoryTypoError }
